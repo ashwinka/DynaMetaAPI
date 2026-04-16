a4e4f7433039f1018211a1ed172289d16316153e
ashwinka <32065323+ashwinka@users.noreply.github.com>
2026-04-16 13:22:34 +0530

    Create UiMetadataBundle.java

---
+++ backend/src/main/java/com/metadyna/metadata/model/UiMetadataBundle.java
@@ -0,0 +1,26 @@
+package com.metadyna.metadata.model;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Builder
+public class UiMetadataBundle {
+
+    private Map<String, Object> formMetadata = new HashMap<>();
+    private JsonNode listingMetadata;
+    private JsonNode lookupMetadata;
+    private JsonNode i18nTrans;
+    private List<ColumnMetadata> fields = new ArrayList<>();
+    private Map<String, List<Map<String, Object>>> codeLists = new HashMap<>();
+
+    public void setCodelists(Map<String, List<Map<String, Object>>> codeLists){
+        this.codeLists = codeLists;
+    }
+}
