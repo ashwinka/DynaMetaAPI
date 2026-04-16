36635da5cfd854ed64564385c648d8208c71d1c1
ashwinka <32065323+ashwinka@users.noreply.github.com>
2026-04-16 13:08:28 +0530

    Create ResourceNotFoundException.java

---
+++ backend/src/main/java/com/metadyna/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.metadyna.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class ResourceNotFoundException extends AppException {
+
+    public ResourceNotFoundException(String message) {
+        super("RESOURCE_NOT_FOUND", message, HttpStatus.NOT_FOUND);
+    }
+}
