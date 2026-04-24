fb8a0729ab52502b3c27d9b70f165b30a9610793
ashwinka <32065323+ashwinka@users.noreply.github.com>
2026-04-24 12:51:17 +0530

    Updated rule engine changes

--- frontend/core/app-metadata.js
+++ frontend/core/app-metadata.js
@@ -43,6 +43,46 @@
 		}
 	}
 	
+	function resolveRenderRulesFieldImpacts(renderRule, triggerFieldImpacts, target, targetType){
+		let triggerFields = renderRule.triggerFields ? renderRule.triggerFields.split('|') : [];
+		
+		for(let flId of triggerFields){
+			if(!triggerFieldImpacts[flId]){
+				triggerFieldImpacts[flId] = {impactedRules:{}, impactedFieldsRender:{}, impactedSectionsRender:{}, impactedFieldReadonly:{}, impactedGridActions:{}, impactedFieldCodelist:{}, impactedFieldLookup:{}};
+			}
+			
+			let currFlImpacts = triggerFieldImpacts[flId];
+			currFlImpacts.impactedRules[renderRule.ruleId] = true;
+			
+			switch(targetType){
+				case 'fieldRender':
+					currFlImpacts.impactedFieldsRender[target] = true;
+					break;
+				case 'readonlyRender':
+					currFlImpacts.impactedFieldReadonly[target] = true;
+					break;				
+				case 'codelistRender':
+					currFlImpacts.impactedFieldCodelist[target] = true;
+					break;
+					
+				case 'lookupRender':
+					currFlImpacts.impactedFieldLookup[target] = true;
+					break;
+					
+				case 'sectionRender':
+					currFlImpacts.impactedSectionsRender[target] = true;
+					break;
+					
+				case 'gridActionsRender':
+					currFlImpacts.impactedGridActions[target] = true;
+					break;
+			}
+			
+		}
+
+		
+	}
+	
 	function resolveListingMd(md, flMdMap){
 		//Resolve Filters
 		let filters = md?.FILTERS_FIELDS || [];
@@ -63,12 +103,24 @@
 		}
 	}
 	
-	function resolveFormMd(fmMd, flMdMap){
+	function resolveFormMd(fmMd, flMdMap, renderRuleMap, triggerFieldImpacts, rlInd){
 		let fields = fmMd.fields || [];
 		
 		for(let f of fields){
 			let flMd = flMdMap[f.fieldId];
 			if(flMd){resolveFieldMd(f, flMd);}
+			
+			if(f.renderCondition && rlInd[f.renderCondition]){
+				let renderRule = rlInd[f.renderCondition];
+				renderRuleMap[renderRule.ruleId] = renderRule;
+				resolveRenderRulesFieldImpacts(renderRule, triggerFieldImpacts, f.fieldId, 'fieldRender');
+			}
+			
+			if(f.readonlyCondition && rlInd[f.readonlyCondition]){
+				let renderRule = rlInd[f.readonlyCondition];
+				renderRuleMap[renderRule.ruleId] = renderRule;
+				resolveRenderRulesFieldImpacts(renderRule, triggerFieldImpacts, f.fieldId, 'readonlyRender');
+			}
 		}
 		
 		let gridFields = fmMd.gridFields || [];
@@ -79,7 +131,20 @@
 		
 		let sections = fmMd.sections || [];
 		for(let sec of sections){
-			resolveFormMd(sec, flMdMap);
+			resolveFormMd(sec, flMdMap, renderRuleMap, triggerFieldImpacts, rlInd);
+			
+			if(sec.renderCondition && rlInd[sec.renderCondition]){
+				let renderRule = rlInd[sec.renderCondition];
+				renderRuleMap[renderRule.ruleId] = renderRule;
+				resolveRenderRulesFieldImpacts(renderRule, triggerFieldImpacts, sec.sectionId, 'sectionRender');
+			}
+			
+			if(sec.gridActionsRender && rlInd[sec.gridActionsRender]){
+				let renderRule = rlInd[sec.gridActionsRender];
+				renderRuleMap[renderRule.ruleId] = renderRule;
+				resolveRenderRulesFieldImpacts(renderRule, triggerFieldImpacts, sec.sectionId, 'gridActionsRender');
+			}
+			
 		}
 	}
 	
@@ -87,9 +152,20 @@
 		let listingMd = mdBundle?.listingMetadata
 		let lkpMd = mdBundle?.lookupMetadata;
 		let fmsMd = mdBundle?.formMetadata?.FORMS || {};
+		let rules = mdBundle?.renderRules || [];
+		let rlInd = {};
 		let flMdMap = {};
 		for(let f of (mdBundle?.fields || [])){
 			flMdMap[f.fieldId] = resolveFieldUiMetadata(f)
+		}
+		
+		for(let r of rules){
+			try{
+				r.ruleExpression = JSON.parse(r.ruleExpression);
+				rlInd[r.ruleId] = r;
+			}catch(err){
+				
+			}
 		}
 		
 		//resolve Listing metadata
@@ -97,7 +173,12 @@
 		resolveListingMd(lkpMd, flMdMap);
 		
 		for(let fmId in fmsMd){
-			resolveFormMd(fmsMd[fmId], flMdMap)
+			let renderRuleMap = {};
+			let triggerFieldImpacts = {};
+			resolveFormMd(fmsMd[fmId], flMdMap, renderRuleMap, triggerFieldImpacts, rlInd)
+
+			fmsMd[fmId].triggerFieldImpacts = triggerFieldImpacts;
+			fmsMd[fmId].renderRules = renderRuleMap;
 		}
 		
 		return mdBundle;
