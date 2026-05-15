969be779660542c3d37023b5319e466b9f31f932
ashwinka <32065323+ashwinka@users.noreply.github.com>
2026-05-15 12:54:37 +0530

    Update AppConstants.java

--- backend/src/main/java/com/metadyna/AppConstants.java
+++ backend/src/main/java/com/metadyna/AppConstants.java
@@ -14,4 +14,32 @@ public class AppConstants {
 
     public static final String SAVE_ISSUE_DB_RULE = "DB_CONSTRAINTS";
 
+
+
+    //AppRule and Rule engine constants
+    public static final String RULE_JSON_LOGIC = "logic";
+    public static final String RULE_JSON_COND = "conditions";
+    public static final String RULE_JSON_EXP = "expressions";
+    public static final String RULE_JSON_AND = "and";
+    public static final String RULE_JSON_OR = "or";
+    public static final String RULE_JSON_LHS_FL = "lhsFieldId";
+    public static final String RULE_JSON_LHS_FN = "lhsFunction";
+    public static final String RULE_JSON_LHS_ANY = "lhsAnyOrAll";
+    public static final String RULE_JSON_RHS = "rhsValue";
+    public static final String RULE_JSON_RHS_FL = "lhsFieldId";
+    public static final String RULE_JSON_OPR = "operator";
+    public static final String RULE_JSON_RHS_FN = "rhsFunction";
+    public static final String RE_OP_EQ = "EQ";
+    public static final String RE_OP_NEQ = "NEQ";
+    public static final String RE_OP_LT = "LT";
+    public static final String RE_OP_LTE = "LTE";
+    public static final String RE_OP_GT = "GT";
+    public static final String RE_OP_GTE = "GTE";
+    public static final String RE_OP_IN = "IN";
+    public static final String RE_OP_NOT_IN = "NOT_IN";
+    public static final String RE_OP_LIKE = "LIKE";
+    public static final String RE_OP_NOT_LIKE = "NOT_LIKE";
+    public static final String RE_OP_REG_EX = "REGEX";
+
+
 }
