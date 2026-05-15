9e52ab7f4af176065e8457bcf831eb8dd96dce3a
ashwinka <32065323+ashwinka@users.noreply.github.com>
2026-05-15 12:45:05 +0530

    Update WorkflowEngine.java

--- backend/src/main/java/com/metadyna/workflowManager/WorkflowEngine.java
+++ backend/src/main/java/com/metadyna/workflowManager/WorkflowEngine.java
@@ -1,1 +1,28 @@
 
+package com.metadyna.workflowManager;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class WorkflowEngine {
+
+    public void initializeWorkflow(String dataRedId, String moduleId, String workflowId) {
+
+    }
+
+    public void initWorkflowTransition(String dataRedId, String moduleId, String workflowId, String activityId) {
+
+    }
+
+    public void completeWorkflowTransition(String dataRedId, String moduleId, String workflowId, String activityId) {
+
+    }
+
+    public void exitWorkflow(String dataRedId, String moduleId, String workflowId) {
+
+    }
+}
